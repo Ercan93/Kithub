97c058ccc2ae319d8be889a03c086106684c5ad5
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-15 19:16:17 +0300

    Create Profile.vue
    and axios modified.

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     name: "Login",
     component: () => import("../views/Login.vue"),
   },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("../views/Profile.vue"),
+  },
 ];
 
 const router = new VueRouter({
