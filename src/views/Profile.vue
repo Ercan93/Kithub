cd2332b461931a4b8a89294b30d7f609360c160b
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-17 18:00:34 +0300

    Router control added in UserProfile page.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,3 +3,24 @@
     <h1>User's profile page</h1>
   </div>
 </template>
+<script>
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      username: null,
+      token: null
+    };
+  },
+  created() {
+    let localToken = localStorage.getItem("kithub-api-user-token");
+    let localUsername = localStorage.getItem("kithub-api-user-username");
+    if (!localToken) {
+      this.$router.push("/");
+    } else {
+      this.username = localUsername;
+      this.token = localToken;
+    }
+  }
+};
+</script>
