5103ff11c5732e61c10bd3a23a72c7c88edc475a
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-17 20:19:15 +0300

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar class="app" app>
+    <v-app-bar class="app-bar" app>
       <h1 class="brand-name">Kithub</h1>
-      <v-btn class="ml-auto" color="#F09819">Profile</v-btn>
+      <span class="ml-auto orange--text" color="#F09819">{{email}}</span>
+      <a>
+        <v-icon class="ml-5" color="#F09819">mdi-exit-run</v-icon>
+      </a>
     </v-app-bar>
 
     <v-navigation-drawer class="navigation" app></v-navigation-drawer>
@@ -12,7 +15,18 @@
     </v-main>
 
     <v-footer class="footer justify-center white--text mb-0" app>
-      <p class="mb-0">Ercan Uzunsakal 2020</p>
+      <a href="https://github.com/Ercan93" target="_blank">
+        <v-icon color="grey">mdi-github</v-icon>
+      </a>
+
+      <p class="mb-0 ml-auto">
+        <v-icon>mdi-vuejs</v-icon>
+        <v-icon>mdi-webpack</v-icon>
+        <v-icon>mdi-language-javascript</v-icon>
+        <v-icon>mdi-api</v-icon>
+        <v-icon>mdi-sass</v-icon>
+        <v-icon>mdi-stack-overflow</v-icon>
+      </p>
     </v-footer>
   </v-app>
 </template>
@@ -31,6 +45,8 @@ export default {
     if (!localToken) {
       this.$router.push("/");
     } else {
+      this.$store.state.userInfo.email = localEmail;
+      this.$store.state.userInfo.token = localToken;
       this.email = localEmail;
       this.token = localToken;
     }
@@ -39,7 +55,7 @@ export default {
 </script>
 <style lang="sass" scoped>
 
-.app
+.app-bar
     background: #16222A
     background: -webkit-linear-gradient(to right, #3A6073, #16222A)
     background: linear-gradient(to right, #3A6073, #16222A)
@@ -59,6 +75,4 @@ export default {
     background: linear-gradient(to right, #480048, #F09819)
 .brand-name
     color: #F09819
-   
-
 </style>
