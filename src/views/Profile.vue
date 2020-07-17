1ae374c5e244fd49f209cf8c64bcf3c6355b266b
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-17 19:31:39 +0300

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar app></v-app-bar>
+    <v-app-bar class="app" app>
+      <h1 class="brand-name">Kithub</h1>
+      <v-btn class="ml-auto" color="#F09819">Profile</v-btn>
+    </v-app-bar>
 
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer class="navigation" app></v-navigation-drawer>
 
-    <v-main>
+    <v-main class="main">
       <v-container fluid></v-container>
     </v-main>
 
-    <v-footer app></v-footer>
+    <v-footer class="footer justify-center white--text mb-0" app>
+      <p class="mb-0">Ercan Uzunsakal 2020</p>
+    </v-footer>
   </v-app>
 </template>
 <script>
@@ -32,3 +37,28 @@ export default {
   }
 };
 </script>
+<style lang="sass" scoped>
+
+.app
+    background: #16222A
+    background: -webkit-linear-gradient(to right, #3A6073, #16222A)
+    background: linear-gradient(to right, #3A6073, #16222A)
+
+.navigation
+    background: #3A6073
+    background: -webkit-linear-gradient(to top, #16222A, #3A6073)
+    background: linear-gradient(to top, #16222A, #3A6073)
+.main
+    background: #3A6073
+    background: -webkit-linear-gradient(to top, #16222A, #3A6073)
+    background: linear-gradient(to top, #16222A, #3A6073)
+
+.footer
+    background: #F09819
+    background: -webkit-linear-gradient(to right, #480048, #F09819)
+    background: linear-gradient(to right, #480048, #F09819)
+.brand-name
+    color: #F09819
+   
+
+</style>
