8f40d716affdceb0baeeba0fe8cc0c84a4688554
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-17 18:57:33 +0300

    Profile.vue skeleton prepared.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <h1>User's profile page</h1>
-  </div>
+  <v-app>
+    <v-app-bar app></v-app-bar>
+
+    <v-navigation-drawer app></v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid></v-container>
+    </v-main>
+
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 <script>
 export default {
