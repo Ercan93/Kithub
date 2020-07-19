23f5917f99bfb2b8cffd4e049ad600f41b9966f6
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-19 16:26:06 +0300

    About page deleted.

--- src/router/index.js
+++ src/router/index.js
@@ -11,15 +11,6 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
     path: "/login",
     name: "Login",
     component: () => import("../views/Login.vue"),
@@ -28,6 +19,10 @@ const routes = [
     path: "/profile",
     name: "Profile",
     component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "*",
+    component: Home,
   },
 ];
 
